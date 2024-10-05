9798659c37cf8f3244bd528591afda59c15f30ea
omondi-paul <omondipaulok@gmail.com>
2024-10-05 07:06:59 +0300

    workout ids images modifications

--- src/components/single-v4/property-gallery/index.js
+++ src/components/single-v4/property-gallery/index.js
@@ -1,67 +1,10 @@
 import React from "react";
 import GalleryBox from "./GalleryBox";
-import RealMapView from "./RealMapView";
 
 const PropertyGallery = ({id}) => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="ps-v4-hero-tab position-relative">
-              <ul
-                className="nav nav-pills justify-content-end"
-                id="pills-tab2"
-                role="tablist"
-              >
-                <li className="nav-item" role="presentation">
-                  <button
-                    className="nav-link active mr10"
-                    id="pills-home-tab"
-                    data-bs-toggle="pill"
-                    data-bs-target="#pills-home"
-                    type="button"
-                    role="tab"
-                    aria-controls="pills-home"
-                    aria-selected="true"
-                  >
-                    <span className="flaticon-images text-white fz20" />
-                  </button>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <button
-                    className="nav-link mr10"
-                    id="pills-profile-tab"
-                    data-bs-toggle="pill"
-                    data-bs-target="#pills-profile"
-                    type="button"
-                    role="tab"
-                    aria-controls="pills-profile"
-                    aria-selected="false"
-                  >
-                    <span className="flaticon-map text-white fz20" />
-                  </button>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <button
-                    className="nav-link"
-                    id="pills-contact-tab"
-                    data-bs-toggle="pill"
-                    data-bs-target="#pills-contact"
-                    type="button"
-                    role="tab"
-                    aria-controls="pills-contact"
-                    aria-selected="false"
-                  >
-                    <span className="flaticon-maps-1 text-white fz20" />
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* End container */}
+
 
       <div className="ps-v4-hero-tab">
         <div className="tab-content overflow-visible" id="pills-tabContent2">
@@ -81,7 +24,6 @@ const PropertyGallery = ({id}) => {
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </>
