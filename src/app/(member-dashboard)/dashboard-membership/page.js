faa87fd691a63aff4b1fcf5619556635949b7a08
omondi-paul <omondipaulok@gmail.com>
2024-10-30 15:26:32 +0300

    membership  overview clean up

--- src/app/(member-dashboard)/dashboard-membership/page.js
+++ src/app/(member-dashboard)/dashboard-membership/page.js
@@ -15,19 +15,11 @@ export const metadata = {
 const DashboardMyProfile = () => {
   return (
     <>
-      {/* Main Header Nav */}
       <DashboardHeader />
-      {/* End Main Header Nav */}
-
-      {/* Mobile Nav  */}
       <MobileMenu />
-      {/* End Mobile Nav  */}
-
-      {/* dashboard_content_wrapper */}
       <div className="dashboard_content_wrapper">
         <div className="dashboard dashboard_wrapper pr30 pr0-xl">
           <SidebarDashboard />
-          {/* End .dashboard__sidebar */}
 
           <div className="dashboard__main pl0-md">
             <div className="dashboard__content bgc-f7">
@@ -35,9 +27,7 @@ const DashboardMyProfile = () => {
                 <div className="col-lg-12">
                   <DboardMobileNavigation />
                 </div>
-                {/* End .col-12 */}
               </div>
-              {/* End .row */}
 
               <div className="row align-items-center pb40">
                 <div className="col-lg-12">
@@ -47,7 +37,6 @@ const DashboardMyProfile = () => {
                   </div>
                 </div>
               </div>
-              {/* End .row */}
 
               <div className="row">
                 <div className="col-xl-12">
@@ -55,38 +44,29 @@ const DashboardMyProfile = () => {
                     <div className="col-xl-7">
                       <ProfileBox />
                     </div>
-                    {/* End ProfileBox */}
 
                     <div className="col-lg-12">
                       <PersonalInfo />
                     </div>
-                    {/* End PersonalInfo */}
                   </div>
-                  {/* End .ps-widget */}
 
                   <div className="ps-widget bgc-white bdrs12 default-box-shadow2 p30 mb30 overflow-hidden position-relative">
                     <h4 className="title fz17 mb30">Social Media</h4>
                     <SocialField />
                   </div>
-                  {/* End .ps-widget */}
 
                   <div className="ps-widget bgc-white bdrs12 default-box-shadow2 p30 mb30 overflow-hidden position-relative">
                     <h4 className="title fz17 mb30">Change password</h4>
                     <ChangePasswordForm />
                   </div>
-                  {/* End .ps-widget */}
                 </div>
               </div>
-              {/* End .row */}
             </div>
-            {/* End .dashboard__content */}
 
             <Footer />
           </div>
-          {/* End .dashboard__main */}
         </div>
       </div>
-      {/* dashboard_content_wrapper */}
     </>
   );
 };
