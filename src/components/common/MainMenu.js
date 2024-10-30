c00e60e76de18e64dd32075f6ca60037854ddf86
omondi-paul <omondipaulok@gmail.com>
2024-10-30 13:02:40 +0300

    workout trainers

--- src/components/common/MainMenu.js
+++ src/components/common/MainMenu.js
@@ -1,58 +1,50 @@
+import { pageItems } from "@/data/navItems";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const MainMenu = () => {
   const pathname = usePathname();
   const [topMenu, setTopMenu] = useState("");
+  const [activeLink, setActiveLink] = useState("");
 
   useEffect(() => {
     if (pathname.includes("/home")) {
       setTopMenu("home");
-    }
-    if (pathname.includes("/trainers")) {
-      setTopMenu("trainers");
+    } else if (pathname.includes("/pages")) {
+      setTopMenu("pages");
     }
   }, [pathname]);
+
+  const handleActive = (link) => {
+    return link.split("/")[1] === pathname.split("/")[1] ? "menuActive" : "";
+  };
+
   return (
     <ul className="ace-responsive-menu">
       <li className="visible_list dropitem">
-        <a className="list-item" href="#" onClick={() => (window.location.href = "/home")}>
+        <a className="list-item" href="#">
           <span className={topMenu === "home" ? "title menuActive" : "title"}>
             Home
           </span>
         </a>
       </li>
 
-      <li className="megamenu_style dropitem">
-        <a className="list-item" href="#" onClick={() => (window.location.href = "/trainers")}>
-          <span className={topMenu === "trainers" ? "title menuActive" : "title"}>
-            Trainers
+      <li className="visible_list dropitem">
+        <a className="list-item" href="#">
+          <span className={topMenu === "pages" ? "title menuActive" : "title"}>
+            Pages
           </span>
         </a>
-      </li>
-
-
-      <li className="megamenu_style dropitem">
-        <a className="list-item" href="#" onClick={() => (window.location.href = "/workout-plans")}>
-          <span className={topMenu === "workout-plans" ? "title menuActive" : "title"}>
-            Workout Plans
-          </span>
-        </a>
-      </li>
-
-      <li className="megamenu_style dropitem">
-        <a className="list-item" href="#" onClick={() => (window.location.href = "/registration")}>
-          <span className={topMenu === "registration" ? "title menuActive" : "title"}>
-            Registration
-          </span>
-        </a>
-      </li>
-      <li className="megamenu_style dropitem">
-        <a className="list-item" href="#" onClick={() => (window.location.href = "/locker-booking")}>
-          <span className={topMenu === "locker-booking" ? "title menuActive" : "title"}>
-            Locker Booking
-          </span>
-        </a>
+        <ul className="sub-menu">
+          {pageItems.map((item, index) => (
+            <li key={index}>
+              <Link className={`${handleActive(item.href)}`} href={item.href}>
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </li>
     </ul>
   );
